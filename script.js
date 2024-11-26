bfc440298b4f27efcb73afa7074eb58f1c44ba83
Diptanu Baidya <129202457+Diptanu12@users.noreply.github.com>
2024-11-26 20:15:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-//your JS code here. If required.
 const inputs = document.querySelectorAll('.code');
 
 inputs.forEach((input, index) => {
     input.addEventListener('input', () => {
-        const value = input.value;
-        if (value.length === 1 && index < inputs.length - 1) {
+        if (input.value.length === 1 && index < inputs.length - 1) {
             inputs[index + 1].focus();
         }
     });
 
-    input.addEventListener('keydown', (event) => {
-        if (event.key === 'Backspace' && input.value === '' && index > 0) {
-            inputs[index - 1].focus();
+    input.addEventListener('keydown', (e) => {
+        if (e.key === 'Backspace' && input.value === '') {
+            if (index > 0) inputs[index - 1].focus();
         }
     });
 });
