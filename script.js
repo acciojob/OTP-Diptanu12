2b58ded3cadd3b9f4fe679ee2758a0cbd207c8fd
Diptanu Baidya <129202457+Diptanu12@users.noreply.github.com>
2024-11-27 21:16:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const inputs = document.querySelectorAll('.code');
 inputs.forEach((input, index) => {
   input.addEventListener('input', () => {
     if (input.value && index < inputs.length - 1) {
-      inputs[index + 1].focus();
+      inputs[index + 1].focus(); 
     }
   });
 
   input.addEventListener('keydown', (e) => {
     if (e.key === 'Backspace') {
       if (input.value) {
-        input.value = ''; 
+        input.value = '';
       } else if (index > 0) {
         inputs[index - 1].focus(); 
       }
